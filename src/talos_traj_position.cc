6d17143d52373297f2a16007b404797d05ec979a
Ramón Calvo González <ramon@ramoncalvo.com>
2020-12-16 12:33:11 +0100

    Generate and publish joint trajectory messages

--- src/talos_traj_position.cc
+++ src/talos_traj_position.cc
@@ -45,6 +45,9 @@ int main(int argc, char *argv[]) {
   ros::init(argc, argv, "rosbag_trajectory_player");
   ros::NodeHandle nh;
 
+  auto pub_l = nh.advertise<trajectory_msgs::JointTrajectory>("/left_leg_controller/command", 100);
+  auto pub_r = nh.advertise<trajectory_msgs::JointTrajectory>("/right_leg_controller/command", 100);
+
   // Get bag file, if not specified, use towr default bag
   std::string name;
   if (argc == 1) {
@@ -69,7 +72,7 @@ int main(int argc, char *argv[]) {
 
   // Prepare trajectory message
   trajectory_msgs::JointTrajectory traj_left, traj_right;
-  traj_left.header.frame_id = "/base_link";
+  traj_left.header.frame_id = "";
   traj_left.header.stamp = ros::Time::now();
   traj_right.header = traj_left.header;
   traj_left.joint_names.emplace_back("leg_left_1_joint");
@@ -84,10 +87,13 @@ int main(int argc, char *argv[]) {
   traj_right.joint_names.emplace_back("leg_right_4_joint");
   traj_right.joint_names.emplace_back("leg_right_5_joint");
   traj_right.joint_names.emplace_back("leg_right_6_joint");
-  traj_left.points.resize(6);
-  traj_right.points.resize(6);
+  // traj_left.points.resize(6);
+  // traj_right.points.resize(6);
 
+  // TODO automatically get frecuency
+  ROS_INFO_STREAM("Calculating joint positions...");
   xpp::InverseKinematicsTalos ik;
+  size_t current_t = 0;
   for (rosbag::MessageInstance const m : view) {
     xpp_msgs::RobotStateCartesian::ConstPtr i =
 	m.instantiate<xpp_msgs::RobotStateCartesian>();
@@ -96,18 +102,36 @@ int main(int argc, char *argv[]) {
       // Perform inverse kinematics
       Eigen::VectorXd q = GetJointAngles(i, ik);
 
+      // Calculate corresponding time for position
+      double time = current_t * 0.01;
+      traj_left.points.emplace_back();
+      traj_right.points.emplace_back();
+      traj_left.points.back().time_from_start = ros::Duration(time);
+      traj_right.points.back().time_from_start = ros::Duration(time);
       // Add values to JointTrajectory
       for (int i = 0; i < 6; ++i) {
 	// Positions
-	traj_left.points.at(i).positions.push_back(q[i]);
+	traj_left.points.back().positions.push_back(q[i]);
       }
       for (int i = 6; i < q.size(); ++i) {
-	traj_right.points.at(i - 6).positions.push_back(q[i]);
+	traj_right.points.back().positions.push_back(q[i]);
       }
     }
+
+    current_t++;
   }
 
-  std::cout << traj_left << "\n";
+  // Publicar ambos mensajes
+  ROS_INFO_STREAM("Publishing messages");
 
+  // Wait for publishers
+  ros::Duration(0.2).sleep();
+
+  pub_l.publish(traj_left);
+  pub_r.publish(traj_right);
+
+  ros::spinOnce();
+
+  ROS_INFO_STREAM("Done.");
   return 0;
 }
